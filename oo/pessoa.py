400691a705ba50a1f6a5ac8f66c7b2a15a3f8dcb
Reginaldo-rsti <reginaldo.rsti@gmail.com>
2021-06-19 21:30:22 -0300

    criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,21 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=42):
+    def __init__(self, *filhos, nome=None, idade=42):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Manoel')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Reginaldo'
-    print(p.nome)
-    print(p.idade)
+    reginaldo = Pessoa(nome='Reginaldo')
+    pedro = Pessoa(reginaldo, nome='Pedro')
+    print(Pessoa.cumprimentar(pedro))
+    print(id(pedro))
+    print(pedro.cumprimentar())
+    print(pedro.nome)
+    print(pedro.idade)
+    print(pedro.filhos)
+    for filho in pedro.filhos:
+        print(filho.nome)
